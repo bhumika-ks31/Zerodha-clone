ed4752e7d6b6636686f6a16d0c0ffeb6cbfd109c
Bhumika Kashyap <tilantajagra@gmail.com>
2025-05-28 18:33:38 +0530

    Initial commit

---
+++ backend/model/PositionsModel.js
@@ -0,0 +1,7 @@
+const { model } = require("mongoose");
+
+const { PositionsSchema } = require("../schemas/positionsSchema");
+
+const PositionsModel = new model("position", PositionsSchema);
+
+module.exports = { PositionsModel };
